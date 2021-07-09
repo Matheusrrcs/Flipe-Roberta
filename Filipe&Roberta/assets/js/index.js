108be4d1913313ff435003a37d3b28fb1372896f
Matheusrrcs <79647787+Matheusrrcs@users.noreply.github.com>
2021-07-09 12:06:34 -0300

    Conserto da galeria e nome title

--- Filipe&Roberta/assets/js/index.js
+++ Filipe&Roberta/assets/js/index.js
@@ -1,13 +1,13 @@
 function atualizarDataHora() {
 
     const now = new Date(); // Data de hoje
-    const past = new Date('2021-03-08'); // Outra data no passado
+    const past = new Date('2021-03-06'); // Outra data no passado
     const diff = Math.abs(now.getTime() - past.getTime()); // Subtrai uma data pela outra
     const days = Math.ceil(diff / (1000 * 60 * 60 * 24)-1); // Divide o total pelo total de milisegundos correspondentes a 1 dia. (1000 milisegundos = 1 segundo).
 
     const html = document.getElementById('sub');
 
-    html.innerText = ` Já se passou ${days} dias . 08.03.2021`
+    html.innerText = ` Já se passaram ${days} dias . 06.03.2021`
 }
 
 atualizarDataHora();
